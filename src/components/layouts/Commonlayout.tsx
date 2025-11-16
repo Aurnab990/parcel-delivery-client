d22641ce3d63ed406d0a78b79e7329908c86e2b0
Aurnab Das <59371873+Aurnab990@users.noreply.github.com>
2025-11-16 12:10:00 +0600

    section changed

--- src/components/layouts/Commonlayout.tsx
+++ src/components/layouts/Commonlayout.tsx
@@ -1,11 +1,9 @@
 import type { ReactNode } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import { Header } from "./Header";
 import SearchSection from "./SearchSection";
 import ServiceHighlights from "./ServiceHighlights";
 import HowItWorks from "./HowItWorks";
-import LiveParcelState from "./LiveParcelState";
 import Testimonials from "./Testimonials";
 import ContactUs from "./ContactUs";
 
@@ -16,11 +14,9 @@ export default function Commonlayout({ children }: IProps) {
   return (
     <div className="min-h-screen flex flex-col ">
       <Navbar />
-      {/* <Header /> */}
       <SearchSection />
       <ServiceHighlights />
       <HowItWorks />
-      {/* <LiveParcelState /> */}
       <Testimonials />
       <ContactUs />
       <div className="grow-1">{children}</div>
